cc5c18e059a1687c6fa19208696c88be14e67918
Ville Ilvonen <vilvo@iki.fi>
2019-03-10 10:19:17 +0200

    py: connect neural net to predict frames of input mjpeg stream

--- py/private_deep_learning/private_deep_learning/cli.py
+++ py/private_deep_learning/private_deep_learning/cli.py
@@ -12,8 +12,15 @@ import syft
 import torchvision
 import numpy
 
+import PIL
+import io
+
 # local modules
 from mjpegc import mjpegc
+
+# CIFAR labels
+model_labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
 
 class Net(torch.nn.Module):
     def __init__(self):
@@ -98,20 +105,43 @@ def test(model, device, test_loader):
         100. * correct / len(test_loader.dataset)))
 
 
-def do_predict(model_filename) -> int:
+def do_predict(model_filename, url) -> int:
 
-    #TODO: refactor and resolve an issue with feeding jpegs to neural net
+    model = Net()
+    model.load_state_dict(torch.load(model_filename))
 
-    """
-    from PIL import Image
-    image = Image.open("kissa.jpg")
-    image = numpy.array(image)  # convert image to numpy array
-    t = torch.from_numpy(image)
-    t = t.unsqueeze(0)
-    model.forward(t)
-    k = model(t)
-    result = os.EX_OK
-    """
+    # transform pipeline for our 640x480 mjpeg stream input jpeg images with random crop to square,
+    # resize to cifar10 dataset sample size 32x32 and finally to normalized tensor
+    transform_pipeline = torchvision.transforms.Compose([torchvision.transforms.RandomCrop(480, 480),
+                                                         torchvision.transforms.Resize((32, 32)),
+                                                         torchvision.transforms.ToTensor(),
+                                                         torchvision.transforms.Normalize((0.5, 0.5, 0.5),
+                                                                                          (0.5, 0.5, 0.5))])
+    for jpg in mjpegc.client(url):
+        # process jpeg through PIL and above transform pipeline
+        # unsqueeze converts our tensor (n channels, height, width) to
+        # pytorch model input tensor (n images, n channels, height, width)
+        img = transform_pipeline(PIL.Image.open(io.BytesIO(jpg))).unsqueeze(0)
+        # wrap input tensor to Variable for prediction
+        img = torch.autograd.Variable(img)
+        prediction = model(img)
+        """
+        # using classic cifar10 set trained model gives us much better than chance (1/10)
+        # prediction at
+
+        Train Epoch: 20 [0/50048 (0%)]	Loss: 0.858092
+        Train Epoch: 20 [12800/50048 (26%)]	Loss: 1.153132
+        Train Epoch: 20 [25600/50048 (51%)]	Loss: 1.031593
+        Train Epoch: 20 [38400/50048 (77%)]	Loss: 0.982400
+
+        Test set: Average loss: 1.1617, Accuracy: 5922/10000 (59%)
+
+        # but still a lot of false positives at 30 frames and predictions per second
+        # TODO: filter single predictions from consecutive to reduce noise
+        """
+        if model_labels[prediction.data.numpy().argmax()] == 'cat':
+            print("the cat is alive")
+
     return os.EX_OK
 
 
@@ -169,9 +199,9 @@ def read_stream(url):
 @click.option('--log_interval', default=200)
 @click.option('--epochs', default=20)
 @click.option('--model_filename', default="federated_trained_cifar10_model.pt")
-@click.option('--predict/-no-predict', default=False)
+@click.option('--predict/-no-predict', default=True)
 @click.option('--generate/--no-generate', default=False)
-@click.option('--stream/--no_stream', default=True)
+@click.option('--stream/--no_stream', default=False)
 @click.option('--stream_url', default="http://localhost:5000", help='url to HTTP MJPEG stream')
 def main(count, batch_size,  test_batch_size, device,
          log_interval, epochs, model_filename, predict, generate, stream, stream_url) -> int:
@@ -179,9 +209,13 @@ def main(count, batch_size,  test_batch_size, device,
     result = os.EX_UNAVAILABLE
 
     try:
-        if stream: result = read_stream(stream_url)
-        if predict: result = do_predict(model_filename)
-        if generate: result = do_generate(count, batch_size, test_batch_size, device, epochs, log_interval, model_filename)
+        if stream:
+            result = read_stream(stream_url)
+        elif predict:
+            result = do_predict(model_filename, stream_url)
+        elif generate:
+            result = do_generate(count, batch_size, test_batch_size,
+                                 device, epochs, log_interval, model_filename)
     except:
         print("Unexpected error:", sys.exc_info())
         result = os.EX_SOFTWARE
